72ab9524989f501e5c28c652d05a2ec4a75e67be
Bisio98 <alberto.bisetti@hotmail.it>
2022-02-10 16:10:08 +0100

    scaffolding-base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('page-title')
+    
+@endsection
+
+@section('main_content')
+    Homepage
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics</title>
+    <title>@yield('page-title')</title>
 </head>
 <body>
     
+    <header>
+        header
+    </header>
+
+    <main>
+        @yield('main_content')
+    </main>
+
+    <footer>
+        footer
+    </footer>
 </body>
 </html>
